55662a76dd090521f865ef31aa1c20f12ed7da8e
Ennio-M <marrese.ennio@gmail.com>
2022-03-23 17:37:59 +0100

    bonus separato da esercizio base

---
+++ bonus/javascript/script.js
@@ -0,0 +1,19 @@
+// ALGORITMO
+
+/*
+A([START]) --> B[/output - Inserisci il tuo nome/]
+B --> C[/input - userName/]
+C --> D[/output - Inserisci il tuo cognome/]
+D --> E[/input - userSurname/]
+E --> F[/output - Qual è il tuo colore preferito?/]
+F --> G[/input - userColor/]
+G --> H[userNewPassword = userName + userSurname + userColor + '21']
+H --> I[/output - 'La tua nuova password è ' + userNewPassword/]
+I --> J([FINE])
+*/
+
+const userName = prompt('Inserisci il tuo nome');
+const userSurname = prompt('Inserisci il tuo cognome');
+const userColor = prompt('Qual è il tuo colore preferito?');
+const userNewPassword = userName + userSurname + userColor + '21';
+document.getElementById('password').innerHTML = ('La tua nuova, sicurissima password è " ' + userNewPassword + ' "')

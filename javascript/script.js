7370741af08192c34e429853b0b8bf0bc89b0fef
Ennio-M <marrese.ennio@gmail.com>
2022-03-23 16:39:41 +0100

    esercizio completato

---
+++ javascript/script.js
@@ -0,0 +1,4 @@
+const userName = prompt('Inserisci il tuo nome');
+const userSurname = prompt('Inserisci il tuo cognome');
+const userColor = prompt('Qual è il tuo colore preferito?');
+document.getElementById('password').innerHTML = ('La tua nuova, sicurissima password è "' + userName + userSurname + userColor + '21"')
